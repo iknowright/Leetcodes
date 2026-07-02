1c41f164699e24a5782f290abf47bb8739595041
iknowright <ikrsrajan@gmail.com>
2026-07-02 16:20:17 +0530

    Time: 0 ms (100.00%) | Memory: 65 MB (50.62%) - LeetSync

---
+++ 344-reverse-string/reverse-string.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public void ReverseString(char[] s) {
+        char temp;
+        int left=0, right=s.Length-1;
+        while(left<right) {
+            temp=s[left];
+            s[left++]=s[right];
+            s[right--]=temp;
+        }
+    }
+}
