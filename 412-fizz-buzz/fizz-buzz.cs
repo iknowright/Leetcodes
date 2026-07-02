2dd15eec5a30cab96dbd016b7fe36c31365f9673
iknowright <ikrsrajan@gmail.com>
2026-07-02 18:15:49 +0530

    Time: 5 ms (63.37%) | Memory: 70.6 MB (6.45%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public IList<string> FizzBuzz(int n) {
+        List<string> res=new();
+        for(int i=1; i<=n; i++) {
+            if(i%3==0 && i%5==0) res.Add("FizzBuzz");
+            else if(i%3==0) res.Add("Fizz");
+            else if(i%5==0) res.Add("Buzz");
+            else res.Add(i.ToString());
+        }
+        return res;
+    }
+}
