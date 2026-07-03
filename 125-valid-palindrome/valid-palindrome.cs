15dd274e1d940d92b5a9c39cb677290565a9a821
Srajan Rai <ikrsrajan@gmail.com>
2026-07-03 20:05:03 +0530

    Time: 38 ms (17.62%) | Memory: 44.8 MB (69.04%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        int left=0, right=s.Length-1;
+        while(left<right) {
+            while(left<right && !char.IsLetterOrDigit(s[left])) left++;
+            while(right>left && !char.IsLetterOrDigit(s[right])) right--;
+            if(char.ToLower(s[left++]) != char.ToLower(s[right--])) return false;
+        }
+        return true;
+    }
+}
