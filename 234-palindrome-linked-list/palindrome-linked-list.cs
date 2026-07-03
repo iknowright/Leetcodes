0119a19ac64d6a9dc399db00e463f30fdccdda43
Srajan Rai <ikrsrajan@gmail.com>
2026-07-04 01:02:06 +0530

    Time: 13 ms (13.47%) | Memory: 62.3 MB (48.57%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cs
+++ 234-palindrome-linked-list/palindrome-linked-list.cs
@@ -13,17 +13,19 @@ public class Solution {
     public bool IsPalindrome(ListNode head) {
         if(head is null) return true;
         ListNode slow=head, fast=head;
-        List<int>left=new(), right=new();
+        List<int>left=new();
         while(fast!=null && fast.next!=null) {
             left.Add(slow.val);
             slow=slow.next;
             fast=fast.next.next;
         }
         if(fast is not null) slow=slow.next;
+        
+        int count=left.Count-1;
         while(slow is not null) {
-            right.Add(slow.val);
+            if(slow.val != left[count--]) return false;
             slow=slow.next;
         }
-        return left.AsEnumerable().Reverse().SequenceEqual(right);
+        return true;
     }
 }
