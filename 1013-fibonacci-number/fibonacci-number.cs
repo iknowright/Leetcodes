132caaf233f0661640f025846506909f2a215755
Srajan Rai <ikrsrajan@gmail.com>
2026-07-05 00:57:52 +0530

    Time: 20 ms (69.29%) | Memory: 28.3 MB (98.73%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int Fib(int n) {
+        if(n <= 1) return n;
+        int a=0, b=1,c=1;
+        for(int i=2; i<=n; i++) {
+            a=b;
+            b=c;
+            c=a+b;
+        }
+        return b;
+    }
+}
