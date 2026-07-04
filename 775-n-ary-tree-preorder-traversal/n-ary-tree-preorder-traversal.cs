27c132f62e4daf7a1074f58a79ab4445e0c4317e
Srajan Rai <ikrsrajan@gmail.com>
2026-07-05 02:36:30 +0530

    Time: 162 ms (66.67%) | Memory: 53.2 MB (72.73%) - LeetSync

---
+++ 775-n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.cs
@@ -0,0 +1,31 @@
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public IList<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val,IList<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+public class Solution {
+    List<int> res=new();
+    public IList<int> Preorder(Node root) {
+        Traversal(root);
+        return res;
+    }
+    public void Traversal(Node root) {
+        if(root is null) return;
+        res.Add(root.val);
+        foreach(Node child in root.children) Traversal(child);
+    }
+}
