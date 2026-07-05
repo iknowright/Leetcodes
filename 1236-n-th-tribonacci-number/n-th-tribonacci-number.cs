e9427dba2bdfaa539d0f917852243935317a6b64
Srajan Rai <ikrsrajan@gmail.com>
2026-07-06 01:57:50 +0530

    Time: 0 ms (100.00%) | Memory: 28.8 MB (74.40%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int Tribonacci(int n) {
+        if(n<2) return n;
+        else if(n is 2) return 1;
+        int a=0,b=1,c=1, d;
+        for(int i=3; i<=n; i++) {
+            d=a+b+c;
+            a=b;
+            b=c;
+            c=d;
+        }
+        return c;
+    }
+}
