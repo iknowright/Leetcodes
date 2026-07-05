89265c3ded8e2c54674b4308a5e78c99db3809c6
Srajan Rai <ikrsrajan@gmail.com>
2026-07-06 01:42:59 +0530

    Time: 58 ms (80.67%) | Memory: 39.5 MB (43.70%) - LeetSync

--- 1205-defanging-an-ip-address/defanging-an-ip-address.cs
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.cs
@@ -1,10 +1,5 @@
 public class Solution {
     public string DefangIPaddr(string address) {
-        string defanged="";
-        foreach(char c in address) {
-            if(c is '.') defanged+="[.]";
-            else defanged+=c;
-        }
-        return defanged;
+        return address.Replace(".", "[.]");
     }
 }
