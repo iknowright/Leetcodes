f6e6faedde9c8a59f848adf5bfbc96a0b82b166d
Srajan Rai <ikrsrajan@gmail.com>
2026-07-07 00:56:59 +0530

    Time: 1 ms (71.43%) | Memory: 39.8 MB (76.19%) - LeetSync

--- 1490-generate-a-string-with-characters-that-have-odd-counts/generate-a-string-with-characters-that-have-odd-counts.cs
+++ 1490-generate-a-string-with-characters-that-have-odd-counts/generate-a-string-with-characters-that-have-odd-counts.cs
@@ -1,6 +1,5 @@
 public class Solution {
     public string GenerateTheString(int n) {
-        if(n==1) return "a";
-        return new string('a', n-2) + new string('b', 1) + (n%2==0?'a':'c');
+        return new string('a', n-1) + (n%2==0?'b':'a');
     }
 }
