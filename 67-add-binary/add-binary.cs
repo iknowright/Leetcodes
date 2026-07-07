48df86ca18f041a122bda208a2fdcb328325cf2f
Srajan Rai <ikrsrajan@gmail.com>
2026-07-08 00:54:33 +0530

    Time: 2 ms (33.82%) | Memory: 43.6 MB (25.73%) - LeetSync

--- 67-add-binary/add-binary.cs
+++ 67-add-binary/add-binary.cs
@@ -1,12 +1,11 @@
 public class Solution {
     public string AddBinary(string a, string b) {
-        int carry=0, i=a.Length-1, j=b.Length-1;
-        int upper, lower, sum;
+        int carry=0, i=a.Length-1, j=b.Length-1, sum;
         string res="";
         while(i>=0 || j>=0 || carry==1) {
-            upper = (i>=0 && a[i--]=='1')? 1:0;
-            lower = (j>=0 && b[j--]=='1')? 1:0;
-            sum = carry + upper + lower;
+            sum=carry;
+            if(i>=0) sum+=a[i--]-'0';
+            if(j>=0) sum+=b[j--]-'0';
             carry= sum>=2? 1:0;
             res = (sum%2).ToString() + res;
         }
