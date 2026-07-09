3ee72a1fea5a96a792aadec2536bc08173595cd3
Srajan Rai <ikrsrajan@gmail.com>
2026-07-10 03:18:59 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (91.65%) - LeetSync

---
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int NumOfStrings(string[] patterns, string word) {
+        int num=0;
+        foreach(string pattern in patterns) {
+            if(word.Contains(pattern)) num++;
+        }
+        return num;
+    }
+}
