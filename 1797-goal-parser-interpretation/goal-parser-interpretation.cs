2389634977b55fcafbd90ee9a048537a82b47f62
Srajan Rai <ikrsrajan@gmail.com>
2026-07-11 01:31:26 +0530

    Time: 60 ms (82.35%) | Memory: 39.8 MB (35.29%) - LeetSync

--- 1797-goal-parser-interpretation/goal-parser-interpretation.cs
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.cs
@@ -3,15 +3,13 @@ public class Solution {
         string res="";
         for(int i=0; i<command.Length; i++) {
             if(command[i]=='G') res+='G';
+            else if(command[i+1]==')') {
+                i++;
+                res+='o';
+            }
             else {
-                if(command[i+1]==')') {
-                    i++;
-                    res+='o';
-                }
-                else {
-                    i+=3;
-                    res+="al";
-                }
+                i+=3;
+                res+="al";
             }
         }
         return res;
