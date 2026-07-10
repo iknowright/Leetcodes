c7b9016d1bb5cb9b676787764cc8278133d45461
Srajan Rai <ikrsrajan@gmail.com>
2026-07-11 01:15:18 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (63.27%) - LeetSync

---
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.cs
@@ -0,0 +1,8 @@
+public class Solution {
+    public bool ArrayStringsAreEqual(string[] word1, string[] word2) {
+        string s1="", s2="";
+        foreach(string word in word1) s1+=word;
+        foreach(string word in word2) s2+=word;
+        return s1==s2;
+    }
+}
