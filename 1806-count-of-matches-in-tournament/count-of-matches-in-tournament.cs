67cd0d2367413afdcefbd0376c772481055f3135
Srajan Rai <ikrsrajan@gmail.com>
2026-07-11 01:57:52 +0530

    Time: 23 ms (27.78%) | Memory: 29.1 MB (25.00%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cs
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cs
@@ -1,11 +1,5 @@
 public class Solution {
-    int matches=0;
     public int NumberOfMatches(int n) {
-        if(n<=2) matches+=n-1;
-        else {
-            matches+=n/2;
-            NumberOfMatches((n+1)/2);
-        }
-        return matches;
+        return n-1;
     }
 }
