df073072527efb13fa4429a645db925923186e80
Srajan Rai <ikrsrajan@gmail.com>
2026-07-12 17:14:30 +0530

    Time: 18 ms (77.35%) | Memory: 28.8 MB (79.31%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int Reverse(int x) {
+        long res=0;
+
+        while(x!=0) {
+            res= res*10 +x%10;
+            if(res<int.MinValue || res>int.MaxValue) return 0;
+            x/=10;
+        }
+        return (int)res;
+    }
+}
