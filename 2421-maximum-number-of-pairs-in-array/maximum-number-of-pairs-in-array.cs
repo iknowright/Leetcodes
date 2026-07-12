647dfeeb8e1dcf40e5d191099736f142089c7d9f
Srajan Rai <ikrsrajan@gmail.com>
2026-07-13 01:26:16 +0530

    Time: 1 ms (64.71%) | Memory: 46.5 MB (58.82%) - LeetSync

---
+++ 2421-maximum-number-of-pairs-in-array/maximum-number-of-pairs-in-array.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int[] NumberOfPairs(int[] nums) {
+        Dictionary<int, int> dict=new();
+        foreach(int num in nums) dict[num]=dict.GetValueOrDefault(num)+1;
+
+        int[] res=new int[2];
+        foreach(int count in dict.Values) {
+            res[0]+=count/2;
+            res[1]+=count%2;
+        }
+        return res;
+    }
+}
