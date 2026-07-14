447f76955d701f37de78a99fd4b33513ddcc5c2e
Srajan Rai <ikrsrajan@gmail.com>
2026-07-14 12:49:46 +0530

    Time: 3 ms (39.13%) | Memory: 48.1 MB (8.70%) - LeetSync

---
+++ 2756-buy-two-chocolates/buy-two-chocolates.cs
@@ -0,0 +1,7 @@
+public class Solution {
+    public int BuyChoco(int[] prices, int money) {
+        Array.Sort(prices);
+        if(prices[0]+prices[1]>money) return money;
+        return money-prices[0]-prices[1];
+    }
+}
