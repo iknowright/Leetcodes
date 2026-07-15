cb7eb335b02650a265c05fb2dd9eb4d613fa759e
Srajan Rai <ikrsrajan@gmail.com>
2026-07-15 21:40:46 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (85.06%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.cs
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsBalanced(TreeNode root) {
+        return Height(root) !=-1;
+    }
+
+    public int Height(TreeNode root) {
+        if(root is null) return 0;
+        if(root.left==null && root.right==null) return 1;
+        int left = Height(root.left), right=Height(root.right);
+        if(left==-1 || right==-1) return -1;
+        if(Math.Abs(left-right)>1) return -1;
+        return Math.Max(left, right)+1;
+    }
+}
