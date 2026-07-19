0703a2b5771c35fd5a691d1038f7ca0a704ac779
Srajan Rai <ikrsrajan@gmail.com>
2026-07-19 10:06:17 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (87.70%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cs
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsValidBST(TreeNode root) {
+        return Valid(root, long.MinValue, long.MaxValue);
+    }
+    public bool Valid(TreeNode root, long min, long max) {
+        if(root is null) return true;
+        else if(root.val>=max || root.val<=min) return false;
+        return Valid(root.left, min, root.val) && Valid(root.right, root.val, max);
+    }
+}
