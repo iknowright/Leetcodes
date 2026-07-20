733c52ef3992b6f9fe63202c39c2383e6450630b
Srajan Rai <ikrsrajan@gmail.com>
2026-07-20 13:11:21 +0530

    Time: 0 ms (100.00%) | Memory: 29 MB (44.12%) - LeetSync

---
+++ 343-integer-break/integer-break.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int IntegerBreak(int n) {
+        if(n==2) return 1;
+        else if(n==3) return 2;
+        else if(n%3==0) return (int) Math.Pow(3, n/3);
+        else if(n%3==1) return (int) (Math.Pow(3, n/3-1)*4);
+        else return (int) (Math.Pow(3, n/3)*2);
+    }
+}
