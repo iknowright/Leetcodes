325cf2eb98c3cbca8fb2595a007ca469183c1263
Srajan Rai <ikrsrajan@gmail.com>
2026-07-21 00:38:49 +0530

    Time: 2 ms (72.73%) | Memory: 43.8 MB (50.00%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.cs
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.cs
@@ -1,10 +1,9 @@
 public class Solution {
     public string MaximumOddBinaryNumber(string s) {
-        int ones=0, zeroes=0;
+        int ones=0;
         foreach(char c in s) {
             if(c is '1') ones++;
-            else zeroes++;
         }
-        return new string('1', ones-1) + new string('0', zeroes) + "1";
+        return new string('1', ones-1) + new string('0', s.Length-ones) + "1";
     }
 }
