9516403836d01013c7fa9e8bb37c3bd3bd63f00c
Srajan Rai <ikrsrajan@gmail.com>
2026-07-20 13:22:17 +0530

    Time: 0 ms (100.00%) | Memory: 28.7 MB (94.44%) - LeetSync

---
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cs
@@ -0,0 +1,6 @@
+public class Solution {
+    public int CountNumbersWithUniqueDigits(int n) {
+        int[] res = {1, 10, 91, 739, 5275, 32491, 168571, 712891, 2345851};
+        return res[n];
+    }
+}
