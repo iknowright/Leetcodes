eb1972299cc8f1db99558412154b601354e1f2bf
Srajan Rai <ikrsrajan@gmail.com>
2026-07-23 02:30:19 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (73.55%) - LeetSync

---
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cs
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode DeleteDuplicates(ListNode head) {
+        ListNode dummy=new(0, head), prev=dummy;
+
+        while(head!=null) {
+            if(head.next!=null && head.val==head.next.val) {
+                while(head.next!=null && head.val==head.next.val) head=head.next;
+
+                prev.next=head.next;
+            }
+            else prev=prev.next;
+
+            head=head.next;
+        }
+        return dummy.next;
+    }
+}
