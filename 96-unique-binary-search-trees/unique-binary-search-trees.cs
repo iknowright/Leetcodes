55a2baa4b311cf1fbf559bc8f9f27db45888f09c
Srajan Rai <ikrsrajan@gmail.com>
2026-07-24 08:03:57 +0530

    Time: 0 ms (100.00%) | Memory: 28.8 MB (70.24%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cs
@@ -0,0 +1,7 @@
+public class Solution {
+    public int NumTrees(int n) {
+        long catalan=1;
+        for(int i=0; i<n; i++) catalan=catalan*(2*(2*i+1))/(i+2);
+        return (int) catalan;
+    }
+}
