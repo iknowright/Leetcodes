2f25f5a6e4c1fc1fa879af0c881454a9843737c8
Srajan Rai <ikrsrajan@gmail.com>
2026-07-25 11:10:36 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (51.52%) - LeetSync

---
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int FindMin(int[] nums) {
+        int left=0, right=nums.Length-1, mid;
+        while(left<right) {
+            mid = left + (right-left)/2;
+            if(nums[mid]>nums[right]) left=mid+1;
+            else right=mid;
+        }
+        return nums[right];
+    }
+}
