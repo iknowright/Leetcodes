ae0f7c8018e1391d1696db5d6250a456ae27261c
Srajan Rai <ikrsrajan@gmail.com>
2026-07-26 14:06:36 +0530

    Time: 415 ms (97.89%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,5 @@
+/* Write your T-SQL query statement below */
+SELECT s.user_id, ROUND(AVG(CAST(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END AS FLOAT)), 2) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c ON s.user_id = c.user_id
+GROUP BY s.user_id;
