0e7529078864cb0033df410eca4fd9bda871257b
Srajan Rai <ikrsrajan@gmail.com>
2026-07-27 05:15:11 +0530

    Time: 317 ms (50.88%) | Memory: 0B (100.00%) - LeetSync

---
+++ 626-exchange-seats/exchange-seats.sql
@@ -0,0 +1,10 @@
+/* Write your T-SQL query statement below */
+SELECT 
+    CASE 
+        WHEN id % 2 = 0 THEN id - 1
+        WHEN id = (SELECT MAX(id) FROM Seat) THEN id
+        ELSE id + 1
+    END AS id,
+    student
+FROM Seat
+ORDER BY id ASC;
