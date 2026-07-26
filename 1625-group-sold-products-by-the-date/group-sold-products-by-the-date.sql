7f0dcfad9b7e1363db507de3b81598e7b40dc6e9
Srajan Rai <ikrsrajan@gmail.com>
2026-07-27 05:09:02 +0530

    Time: 739 ms (75.68%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1625-group-sold-products-by-the-date/group-sold-products-by-the-date.sql
@@ -0,0 +1,12 @@
+/* Write your T-SQL query statement below */
+WITH DistinctProducts AS (
+    SELECT DISTINCT sell_date, product
+    FROM Activities
+)
+SELECT 
+    sell_date, 
+    COUNT(product) AS num_sold,
+    STRING_AGG(product, ',') WITHIN GROUP (ORDER BY product ASC) AS products
+FROM DistinctProducts
+GROUP BY sell_date
+ORDER BY sell_date;
