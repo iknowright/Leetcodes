efb87fde1b0de763eed1ae6e53a6e556523eb574
Srajan Rai <ikrsrajan@gmail.com>
2026-07-28 07:14:41 +0530

    Time: 0 ms (100.00%) | Memory: 50 MB (79.39%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int SingleNonDuplicate(int[] nums) {
+        int left=0, right=nums.Length-1, mid;
+        while(left<right) {
+            mid= left + (right-left)/2;
+            if(mid%2==1) mid--;
+            
+            if(nums[mid]==nums[mid+1]) left=mid+2;
+            else right=mid;
+        }
+        return nums[left];
+    }
+}
