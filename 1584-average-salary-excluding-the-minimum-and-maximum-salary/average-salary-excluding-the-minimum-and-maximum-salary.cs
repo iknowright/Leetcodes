b03bb517bef150dde7000c5c7ecd3ccf082c9a48
Srajan Rai <ikrsrajan@gmail.com>
2026-07-30 19:15:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (13.73%) - LeetSync

--- 1584-average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.cs
+++ 1584-average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.cs
@@ -1,10 +1,11 @@
 public class Solution {
     public double Average(int[] salary) {
-        int max=salary[0], min=salary[0];
+        int max=salary[0], min=salary[0], sum=0;
         foreach(int sal in salary) {
             max = Math.Max(max, sal);
             min = Math.Min(min, sal);
+            sum+= sal;
         }
-        return (salary.Sum()-min-max)/(double)(salary.Length-2);
+        return (sum-min-max)/(double)(salary.Length-2);
     }
 }
