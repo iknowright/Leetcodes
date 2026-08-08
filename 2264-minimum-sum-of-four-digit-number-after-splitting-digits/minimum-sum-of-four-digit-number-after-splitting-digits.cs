43140b77ad5695f2035301756b328d126da2d4c1
Srajan Rai <ikrsrajan@gmail.com>
2026-08-08 13:49:30 +0530

    Time: 1 ms (92.31%) | Memory: 29.4 MB (40.38%) - LeetSync

---
+++ 2264-minimum-sum-of-four-digit-number-after-splitting-digits/minimum-sum-of-four-digit-number-after-splitting-digits.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int MinimumSum(int num) {
+        int[] arr= new int[4];
+        int i=0;
+        while(num!=0) {
+            arr[i++] = num%10;
+            num/=10;
+        }
+        Array.Sort(arr);
+        return (arr[0]+arr[1])*10 + arr[2]+arr[3];
+    }
+}
