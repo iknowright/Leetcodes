f2b3b094d82d93813d02c509a574375a678537c1
Srajan Rai <ikrsrajan@gmail.com>
2026-08-09 02:00:07 +0530

    Time: 2 ms (84.34%) | Memory: 41.1 MB (85.54%) - LeetSync

---
+++ 1137-height-checker/height-checker.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int HeightChecker(int[] heights) {
+        int res=0;
+        int[] expected = (int[]) heights.Clone();
+        Array.Sort(expected);
+        for(int i=0; i<heights.Length; i++) {
+            if(heights[i]!=expected[i]) res++;
+        }
+        return res;
+    }
+}
