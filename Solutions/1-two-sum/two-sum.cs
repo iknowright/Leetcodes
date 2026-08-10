fd571d5bdf309db5da780b9665be892309a26295
Srajan <ikrsrajan@gmail.com>
2026-08-10 16:58:41 +0530

    1. Two Sum

--- Solutions/1-two-sum/two-sum.cs
+++ Solutions/1-two-sum/two-sum.cs
@@ -1,12 +1,12 @@
 public class Solution {
-    public int[] TwoSum(int[] nums, int target) {
-        Dictionary<int, int> pairs = new();
+    public static int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> pairs = [];
         for(int i=0; i<nums.Length; i++) {
             if(pairs.TryGetValue(target-nums[i], out int val)) {
-                return new int[] {i, val};
+                return [i, val];
             }
             else pairs[nums[i]]=i;
         }
-        return new int[] {-1, -1};
+        return [-1, -1];
     }
 }
