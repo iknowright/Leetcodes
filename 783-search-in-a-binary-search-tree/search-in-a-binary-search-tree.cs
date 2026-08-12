bc0abc8854a8890a298aad3231fa1e204737f9eb
Srajan Rai <ikrsrajan@gmail.com>
2026-08-12 09:38:59 -0700

    Time: 0 ms (100.00%) | Memory: 51.4 MB (59.59%) - LeetSync

---
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cs
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode SearchBST(TreeNode root, int val) {
+        while(root is not null && root.val !=val) {
+            root = (root.val>val)? root.left : root.right;
+        }
+        return root;
+    }
+}
