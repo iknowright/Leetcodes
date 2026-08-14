0bb9b763b1dbd98b1e0a444d31faa616c098e13c
Srajan Rai <ikrsrajan@gmail.com>
2026-08-14 14:05:16 -0700

    Time: 0 ms (100.00%) | Memory: 28.8 MB (71.43%) - LeetSync

---
+++ 3371-harshad-number/harshad-number.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int SumOfTheDigitsOfHarshadNumber(int x) {
+        int temp=x, sum=0;
+        while(temp !=0) {
+            sum += temp%10;
+            temp/=10;
+        }
+        return (x%sum==0)? sum:-1;
+    }
+}
