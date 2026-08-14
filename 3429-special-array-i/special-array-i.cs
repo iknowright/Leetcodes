2e6944c0b3b405223b9810c41e4a51b887283ddb
Srajan Rai <ikrsrajan@gmail.com>
2026-08-14 14:25:47 -0700

    Time: 0 ms (100.00%) | Memory: 46.2 MB (14.29%) - LeetSync

---
+++ 3429-special-array-i/special-array-i.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public bool IsArraySpecial(int[] nums) {
+        if(nums.Length==1) return true;
+        for(int i=1; i<nums.Length; i++) {
+            if(nums[i]%2==nums[i-1]%2) return false;
+        }
+        return true;
+    }
+}
