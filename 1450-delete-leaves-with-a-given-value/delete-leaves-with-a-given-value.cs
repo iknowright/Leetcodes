bec2a073052d1624f6a4b9c87966906a30e7949f
Srajan Rai <ikrsrajan@gmail.com>
2026-08-17 02:32:21 -0700

    Time: 0 ms (100.00%) | Memory: 45.6 MB (9.52%) - LeetSync

---
+++ 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.cs
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode RemoveLeafNodes(TreeNode root, int target) {
+        if(root is null) return null;
+
+        root.left = RemoveLeafNodes(root.left, target);
+        root.right = RemoveLeafNodes(root.right, target);
+        if(root.val == target && (root.left is null && root.right is null)) return null;
+        else return root;
+    }
+}
