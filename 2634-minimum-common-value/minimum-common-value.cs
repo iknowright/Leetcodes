0baee9fc8f8cfb519be66784c96b7226b5aa2755
Srajan Rai <ikrsrajan@gmail.com>
2026-08-19 09:46:25 -0700

    Time: 0 ms (100.00%) | Memory: 67.3 MB (47.00%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cs
+++ 2634-minimum-common-value/minimum-common-value.cs
@@ -1,7 +1,7 @@
 public class Solution {
     public int GetCommon(int[] nums1, int[] nums2) {
         int l1 = nums1.Length, l2 = nums2.Length;
-        int i=0, j=0, k=0;
+        int i=0, j=0;
         while(i<l1 && j<l2) {
             if(nums1[i]==nums2[j]) return nums1[i];
             else if(nums1[i]>nums2[j]) j++;
