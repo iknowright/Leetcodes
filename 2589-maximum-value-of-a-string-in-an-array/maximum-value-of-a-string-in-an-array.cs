a6716b15f0ffec9fe89f6594739c56b0a8c6ce76
Srajan Rai <ikrsrajan@gmail.com>
2026-09-13 07:02:01 -0700

    Time: 0 ms (100.00%) | Memory: 41.4 MB (26.92%) - LeetSync

---
+++ 2589-maximum-value-of-a-string-in-an-array/maximum-value-of-a-string-in-an-array.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int MaximumValue(string[] strs) {
+        int max=0;
+        foreach(string s in strs) {
+            if(int.TryParse(s, out int val)) {
+                max = Math.Max(max, val);
+            }
+            else max = Math.Max(max, s.Length);
+        }
+        return max;
+    }
+}
